bcf52ef197bddd01645fb5fc9e95ba84de8cd03a
albert <albert@3new.com>
2023-04-08 11:42:31 +0800

    完成 enum

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,10 +2,12 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -38,6 +40,21 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        // 使用模式匹配获得每个枚举中的值
+        match message {
+            Message::ChangeColor(rgb) => {
+                self.color = rgb;
+            },
+            Message::Echo(s) => {
+                println!("{}", s);
+            },
+            Message::Move(p) => {
+                self.position = p;
+            },
+            Message::Quit => {
+                self.quit = true;
+            },
+        }
     }
 }
 
