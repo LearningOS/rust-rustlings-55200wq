b18b3aea9448ce2110842e96a418cd68b04552ff
wangqing <572264225@qq.com>
2023-04-14 00:51:38 +0800

    完成 lifetime

--- exercises/lifetimes/lifetimes1.rs
+++ exercises/lifetimes/lifetimes1.rs
@@ -7,9 +7,9 @@
 //
 // Execute `rustlings hint lifetimes1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// 标记生命周期
 
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
--- exercises/lifetimes/lifetimes3.rs
+++ exercises/lifetimes/lifetimes3.rs
@@ -4,11 +4,11 @@
 //
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// 结构体中是引用的成员都要标注生命周期
 
-struct Book {
-    author: &str,
-    title: &str,
+struct Book<'a> {
+    author: &'a str,
+    title: &'a str,
 }
 
 fn main() {
